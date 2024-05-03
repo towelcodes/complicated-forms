0d10585f943d766c69c8460ead4dd07f0668bb06
Teatowel <tea@towel.sh>
2024-05-03 15:09:06 +0000

    Initial (non-working) commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let complicatedForm = new ComplicatedForm(document, "/lib");
+
+function initLib() {
+    new ComplicatedForm(document, "/lib");
+}
+
+// debug function
+function addQ() {
+    complicatedForm.addQuestion();
+}
+
+function createQ() {
+    complicatedForm.createQuestion();
+}
